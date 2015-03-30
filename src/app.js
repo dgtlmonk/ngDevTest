c8bbb6aabb3666988cdf348a8b659c5ace4398cb
Louie Marcel Lontoc <maui.lontoc@sumofy.me>
2015-03-30 12:34:28 +0800

    louie lontoc test

--- src/app.js
+++ src/app.js
@@ -16,28 +16,72 @@
          })
          .state('list',{
              url:'/list',
+             controller:'app.controller',
+             controllerAs:'vm',
              templateUrl:'view/people-list.html'
          })
 
  }).controller('app.controller', appController);
-    function appController(resourceAPI) {
+    function appController(resourceAPI, $scope) {
         /*resourceAPI.getData().then(res){
             console.log(res);
         }*/
 
         var vm = this;
+        var storedPeople = resourceAPI.getStoredData();
         vm.init = init;
 
-        vm.init()
+        vm.init();
+
+        console.log(resourceAPI.getStoredData());
 
         function init() {
-            resourceAPI.getData().then(function (res) {
-                console.log(res);
-            }, function (reason) {
-                console.log('Error getting data from resource! ', reason);
-            })
-
+            if(storedPeople.length > 0) {
+                $scope.people = storedPeople;
+            } else {
+                resourceAPI.getData().then(function (res) {
+                    $scope.people = res.data;
+                    resourceAPI.storeData(res.data);
+                    console.log(resourceAPI.getStoredData());
+                }, function (reason) {
+                    console.log('Error getting data from resource! ', reason);
+                });
+            }
         }
+
+        $scope.personData = {};
+        $scope.nameExists = false;
+
+        $scope.addPerson = function() {
+            if(checkIfNameExists($scope.personData.name)) {
+                $scope.people.push($scope.personData);
+                resourceAPI.storeData($scope.people);
+                $scope.clearForm();
+                $scope.nameExists = false;
+                console.log(resourceAPI.getStoredData());
+            } else {
+                $scope.nameExists = true;
+            }
+        }
+
+        $scope.clearForm = function() {
+            $scope.personData = {};
+            $scope.personForm.$setPristine();
+        }
+
+        function checkIfNameExists(name) {
+            var isValid = true;
+            if($scope.people.length > 0) {
+                for(var idx in $scope.people) {
+                    if(name == $scope.people[idx].name) {
+                        isValid = false;
+                    }
+                }
+            }
+
+            return isValid;
+        }
+
     }
 
 
--- src/resource.js
+++ src/resource.js
@@ -4,12 +4,24 @@
     module.factory('resourceAPI', resourceAPI);
 
     function resourceAPI($http) {
+        var people = [];
+
         return {
-            getData: getData
+            getData: getData,
+            storeData: storeData,
+            getStoredData: getStoredData
+        }
+
+        function storeData(storePeople) {
+            people = storePeople;
         }
 
         function getData() {
-            return $http.get("/resource/people.json")
+            return $http.get("/resource/people.json");
+        }
+
+        function getStoredData() {
+            return people;
         }
     }
 
